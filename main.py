5da9320d6b9be98c6effea9f7f544884959e8029
sjt-moon <harry.jt.song@gmail.com>
2018-11-28 18:13:24 -0800

    smoke test

---
+++ main.py
@@ -0,0 +1,11 @@
+from pipeline import Pipeline
+from helper import loader
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+p = Pipeline(base_model_path='../facegen/1/1_0001.obj')
+lsfm, logs = p.run(input_path='../facegen/')
+
+loader.save(lsfm, os.path.join(p.output_path, "lsfm"))
+loader.save(lsfm, os.path.join(p.output_path, "lsfm-training_logs"))
